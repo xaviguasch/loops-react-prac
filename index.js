cc66fbd294854cdb33a72856c1a823c2566b3e5c
xaviguasch <xaviguasch@gmail.com>
2019-04-27 17:40:56 +0200

    starter files

---
+++ index.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Loops</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
